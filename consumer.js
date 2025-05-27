19e5da983b398ca20e86e8f5ce1b57dc9e844760
กฤต <112327677+biskitsx@users.noreply.github.com>
2025-05-27 16:58:38 +0700

    feat: init project

---
+++ consumer.js
@@ -0,0 +1,51 @@
+const amqp = require("amqplib");
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "rootpassword",
+  database: "orders",
+});
+
+connection.connect();
+
+const sleep = (milliseconds) => {
+  return new Promise((resolve) => setTimeout(resolve, milliseconds));
+};
+
+async function receiveOrders() {
+// เชื่อมต่อกับ RabbitMQ
+  const conn = await amqp.connect("amqp://mikelopster:password@localhost:5672");
+    // สร้าง channel
+  const channel = await conn.createChannel();
+
+  const queue = "orders-new";
+  await channel.assertQueue(queue, { durable: true });
+  channel.prefetch(1); // รับ message ทีละ 1 ตัว
+
+  let counter = 0;
+
+  channel.consume(queue, async (msg) => {
+    try {
+        const order = JSON.parse(msg.content.toString());
+        console.log(" [x] Received %s", order);
+   
+        await sleep(1000);
+   
+        const sql = "INSERT INTO orders SET ?";
+        connection.query(sql, order, (error, results) => {
+          if (error) throw error;
+            console.log(`Order ${results.insertId} saved to database, countrer = ${counter}`);
+            counter++;
+            channel.ack(msg);
+        });
+    
+          // บอกว่าได้ message แล้ว
+        } catch (error) {
+          console.log("Error:", error.message);
+        }
+      });
+}
+
+receiveOrders();
---
+++ producer.js
@@ -0,0 +1,30 @@
+const amqp = require("amqplib");
+const { v4: uuidv4 } = require("uuid");
+
+async function sendOrder(order) {
+  const connection = await amqp.connect("amqp://mikelopster:password@localhost:5672");
+  const channel = await connection.createChannel();
+
+  const queue = "orders-new";
+
+  // เขียนลง disk เอาไว้ กรณีที่ queue ดับ
+  await channel.assertQueue(queue, { durable: true });
+
+  // ใส่ persistent + durable จะได้ข้อมูล queue เดิมออกมาได้
+  channel.sendToQueue(queue, Buffer.from(JSON.stringify(order)), { persistent: true });
+  console.log(" [x] Sent %s", order);
+
+  setTimeout(() => {
+    connection.close();
+    process.exit(0);
+  }, 500);
+}
+
+for (let i = 0; i < 50; i++) {
+  const order = {
+    orderNumber: uuidv4(),
+    product: Math.random().toString(36).substring(7),  
+    quantity: Math.floor(Math.random() * 10) + 1,
+  };
+  sendOrder(order);
+}
